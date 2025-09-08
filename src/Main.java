e8e84f36b9d99a47a30333bca4bce2483b35b0c0
THIAGO COSTA ALVES DA SILVA <25203656@CFP127ALUNO.LOCAL>
2025-09-08 09:45:35 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import javax.print.attribute.standard.Media;
+
+public class Main {
+    public static void main (String[] Args) {
+        System.out.println("Iniciando...");
+
+        media mediaponderada = new media();
+
+        mediaponderada.obterDados();
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
